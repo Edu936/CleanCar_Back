513795bdab9e024818da09376b02a39af39e5f1e
Eduardo Vieira <167880647+Edu936@users.noreply.github.com>
2025-03-26 22:18:58 -0300

    include CleanCar_Domain

---
+++ CleanCar_Domain/Entities/Cities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCar_Domain.Entities
+{
+    public class Cities
+    {
+        public Guid Id { get; init; }
+        public string NameCity { get; private set; }
+        public string StateName { get; private set; }
+        public Cities(string nameCity, string stateName)
+        {
+            Id = Guid.NewGuid();
+            NameCity = nameCity;
+            StateName = stateName;
+        }
+    }
+}
